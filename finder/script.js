c01bd4b69918f88d412320fbe0a99bfd505d0928
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-07 09:38:07 +0500

    split

--- finder/script.js
+++ finder/script.js
@@ -2,4 +2,10 @@ function checkword() {
     var text = prompt("Enter the text");
     var wordtocheck = prompt("Enter the word for check");
     var result = alert();
+
+    var words = text.split(/\s+/);
+
+    if (words.length < 3) {
+        alert("The text does not contain at least three words");
+    }
 }
