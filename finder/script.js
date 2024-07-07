1d278d146b0741d285abeb2bef81fab6b15a90c0
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-07 21:20:38 +0500

    1-task is done

--- finder/script.js
+++ finder/script.js
@@ -3,19 +3,22 @@
 
     function checkword() {
     var words = text.split(/\s+/);
-
     if (words.length < 3) {
         alert("The text does not contain at least three words");
     }
-
-    var thirdword = words[2];
-
-    if (thirdword.startsWith(wordtocheck)) {
-        alert(`The third word "${thirdword}" starts with "${wordtocheck}".`);
+    if (wordtocheck.charCodeAt >= 65 && wordtocheck.charCodeAt <= 90) {
+        wordtochecklower = wordtocheck.toLowerCase();
+        var thirdword = words[2].toLowerCase();
     } else {
-        alert(`The third word "${thirdword}" does not  starts with "${wordtocheck}".`);
+        wordtochecklower = wordtocheck.toUpperCase();
+        var thirdword = words[2].toUpperCase();
+
+    }
+    if (thirdword.startsWith(wordtochecklower)) {
+        alert(`The third word "${thirdword}" starts with "${wordtochecklower}".`);
+    } else {
+        alert(`The third word "${thirdword}" does not  starts with "${wordtochecklower}".`);
     }
 }
-
 var result = checkword();
 // alert(result);
